5ce6e693c7da1f5e4eb72ac57e400a1fa4420b09
Krzysztof Kamiński <bardak_widz@wp.pl>
2020-06-04 14:50:55 +0200

    basic js and css added

---
+++ find_your_goal.js
@@ -0,0 +1,10 @@
+const canvas = document.getElementById('canvas');
+
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
+
+const ctx = canvas.getContext('2d');
+ctx.fillStyle = 'black';
+ctx.fillRect(0, 0, canvas.width, canvas.height);
+
+const goalSize = .01 * Math.ceil(canvas.width);
