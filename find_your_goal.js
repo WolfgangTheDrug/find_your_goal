beb4ebf27b2dd985f5e2d840e06680fc21612cdd
Krzysztof Kamiński <bardak_widz@wp.pl>
2020-06-04 15:32:34 +0200

    click detection

--- find_your_goal.js
+++ find_your_goal.js
@@ -1,10 +1,29 @@
+const palette = {
+    yellow: "#FFFF66",
+    blue: "#00FEFC",
+    brown: "#494738",
+    pink: "#AEAC9A"
+}
+
 const canvas = document.getElementById('canvas');
 
 canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
 
 const ctx = canvas.getContext('2d');
-ctx.fillStyle = 'black';
+ctx.fillStyle = palette.brown;
 ctx.fillRect(0, 0, canvas.width, canvas.height);
 
 const goalSize = .01 * Math.ceil(canvas.width);
+// The goal will be a square with side length being equal to 1% of the window's width.
+
+ctx.beginPath();
+ctx.fillStyle = palette.yellow;
+ctx.fillRect(0, 0, canvas.width, canvas.height);
+ctx.stroke();
+
+function mouseClickPosition(e) {
+    console.log(e.clientX, e.clientY);
+}
+
+canvas.addEventListener("click", mouseClickPosition);
